e2a4ee556f60f0789ca51baa0a8dfebcdd6c054f
Andru50 <hectorandres164@hotmail.com>
2025-06-01 16:55:24 -0500

    actulizaciuon

--- views/clientes/index.php
+++ views/clientes/index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Clientes</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
+
     <div class="container mt-5">
         <h1>Lista de Clientes</h1>
         <a href="index.php?action=create" class="btn btn-primary mb-3">Agregar Cliente</a>
@@ -22,9 +14,9 @@
             <tbody>
                 <?php foreach ($clientes as $cliente): ?>
                 <tr>
-                    <td><?= $cliente['id_cliente'] ?></td>
-                    <td><?= $cliente['nombre_cliente'] ?></td>
-                    <td><?= $cliente['direccion'] ?></td>
+                    <td><?= htmlspecialchars($cliente['id_cliente']) ?></td>
+                    <td><?=htmlspecialchars($cliente['nombre_cliente']) ?></td>
+                    <td><?=htmlspecialchars($cliente['direccion']) ?></td>
                     <td>
                         <a href="index.php?action=edit&id=<?= $cliente['id_cliente'] ?>" class="btn btn-warning btn-sm">Editar</a>
                         <a href="index.php?action=delete&id=<?= $cliente['id_cliente'] ?>" class="btn btn-danger btn-sm">Eliminar</a>
@@ -34,5 +26,3 @@
             </tbody>
         </table>
     </div>
-</body>
-</html>
